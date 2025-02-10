8e866a524019f3a9818fdd902a3f3da66e17cdca
JaimeGHE <jaimeghe2@gmail.com>
2025-02-10 18:14:49 +0000

    Contact List

---
+++ src/front/js/pages/Error404.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+export const Error404 = () => {
+
+    return (
+        <div className="container">
+            <h1 className="display-5 text-center">
+                Error:
+                Page not found
+            </h1>
+        </div>
+    )
+}
